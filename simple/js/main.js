c0f2ae0465c8945c360d83b4426e136e5b56b32b
ClemLya <clementlyara72@gmail.com>
2021-06-04 19:43:14 +0200

    V1.0.1
    Ajout de templates

---
+++ simple/js/main.js
@@ -0,0 +1,34 @@
+;(function () {
+	
+	'use strict';
+
+
+
+	// iPad and iPod detection	
+	var isiPad = function(){
+		return (navigator.platform.indexOf("iPad") != -1);
+	};
+
+	var isiPhone = function(){
+	    return (
+			(navigator.platform.indexOf("iPhone") != -1) || 
+			(navigator.platform.indexOf("iPod") != -1)
+	    );
+	};
+
+	var parallax = function() {
+		$(window).stellar({
+			horizontalScrolling: false,
+			hideDistantElements: false, 
+			responsive: true
+
+		});
+	};
+
+	// Document on load.
+	$(function(){
+		parallax();
+	});
+
+
+}());
